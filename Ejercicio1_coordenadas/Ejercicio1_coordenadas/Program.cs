d89a22b3723ad3b06cc319dcfb2851e33f737a68
Héctor Alfredo Mosquera Coche <mosquerah59@gmail.com>
2024-09-01 18:56:22 -0500

    Guardamos avances de mi código

--- Ejercicio1_coordenadas/Ejercicio1_coordenadas/Program.cs
+++ Ejercicio1_coordenadas/Ejercicio1_coordenadas/Program.cs
@@ -1,7 +1,19 @@
-﻿internal class Program
+﻿using System.Net;
+
+public class Program
 {
+    // Lea las coordenadas de dos puntos y que muestre la distancia entre los dos puntos
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Lea las coordenadas de dos puntos y que muestre la distancia entre los dos puntos
+
+        //DATOS DE ENTRADA (INPUTS)
+        //Declarar las variables. En este caso son 4:
+        double X1, X2, Y1, Y2, d;
+
+        //Vamos a inicializar mis variables pidiendole sus valores por pantalla:
+        Console.Write("Por favor ingrese el valor de X1: ");
+        X1 = Convert.ToDouble(Console.ReadLine ())
+
     }
 }
